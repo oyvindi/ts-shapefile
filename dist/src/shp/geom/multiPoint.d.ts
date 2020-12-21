309e2dd451505e653748c6f7d4d49443f932d41e
oyvindi <oyvind.idland@gmail.com>
2020-12-21 12:04:17 +0100

    Removed accidentally added dist

--- dist/src/shp/geom/multiPoint.d.ts
+++
@@ -1,9 +0,0 @@
-import { Coordinate } from "./coordinate";
-import { ShapeType, ShpGeometryBase } from "./geometry";
-import { GeoJsonGeom } from "./geoJson";
-export declare type ShpMultiPointType = ShapeType.MultiPoint | ShapeType.MultiPointZ | ShapeType.MultiPointM;
-export declare class ShpMultiPoint extends ShpGeometryBase {
-    readonly points: Coordinate[];
-    constructor(type: ShpMultiPointType);
-    toGeoJson(): GeoJsonGeom;
-}
--- dist/src/shp/geom/null.d.ts
+++
@@ -1,5 +0,0 @@
-import { ShpGeometryBase } from "./geometry";
-import { GeoJsonGeom } from "./geoJson";
-export declare class ShpNullGeom extends ShpGeometryBase {
-    toGeoJson(): GeoJsonGeom;
-}
