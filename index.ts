8e37f4d6f015dc272258b04cdb0b7051145b3067
oyvindi <oyvind.idland@gmail.com>
2021-02-08 14:37:04 +0100

    Misc corrections in package.json / rollup.config / tsconfig

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { type } from "os";
-
 export * from "./src/shapeFeature";
 export * from "./src/shapeFeatureReader";
 export * from "./src/shp/geom/geometry";
@@ -10,4 +8,4 @@ export * from "./src/dbf/dbfTypes";
 export { LinerarRing, ShpPolygon } from "./src/shp/geom/polygon";
 export { LineString, ShpPolyLine } from "./src/shp/geom/polyLine";
 export { ShpPoint } from "./src/shp/geom/point";
-export { ShpHeader } from "./src/shp/shapeTypes";
+export type { ShpHeader } from "./src/shp/shapeTypes";
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import commonjs from "@rollup/plugin-commonjs";
 import del from "rollup-plugin-delete";
 import json from "@rollup/plugin-json";
@@ -7,14 +8,31 @@ import packageJson from "./package.json";
 
 export default {
   input: "index.ts",
-  output: {
+  output: [{
     file: packageJson.publishConfig.browser,
     name: packageJson.name,
-    format: "umd",
+    format: "umd"
+  }, 
+  {
+    file: packageJson.publishConfig.module,
+    name: packageJson.name,
+    format: "es",
+    sourcemap: true,
   },
+  {
+    file: packageJson.publishConfig.main,
+    name: packageJson.name,
+    format: "cjs",
+    sourcemap: true,
+  }
+  ],
   plugins: [
     del({ targets: "dist/*", runOnce: true }),
-    nodeResolve([".ts"]),
+    nodeResolve({
+      preferBuiltins : false,
+      extensions : [".ts"]
+    }),  //
+    nodePolyfills(),
     typescript({ abortOnError: true, sourcemap: true, useTsconfigDeclarationDir: true }),
     commonjs(),
     json(),
