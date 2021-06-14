0d47f239e98c615a461bc8bd0938d4c96fa307e6
Thorfinn <thsorens@gmail.com>
2021-06-14 10:18:30 +0200

    improve deploy package

--- rollup.config.js
+++ rollup.config.js
@@ -4,27 +4,29 @@ import nodePolyfills from 'rollup-plugin-node-polyfills';
 import commonjs from '@rollup/plugin-commonjs';
 import del from 'rollup-plugin-delete';
 import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
 import packageJson from './package.json';
 
 export default {
   input: 'index.ts',
-  output: [{
-    file: packageJson.publishConfig.browser,
-    name: packageJson.name,
-    format: 'umd'
-  },
-  {
-    file: packageJson.publishConfig.module,
-    name: packageJson.name,
-    format: 'es',
-    sourcemap: true
-  },
-  {
-    file: packageJson.publishConfig.main,
-    name: packageJson.name,
-    format: 'cjs',
-    sourcemap: true
-  }
+  output: [
+    {
+      file: packageJson.publishConfig.browser,
+      name: packageJson.name,
+      format: 'umd'
+    },
+    {
+      file: packageJson.publishConfig.module,
+      name: packageJson.name,
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: packageJson.publishConfig.main,
+      name: packageJson.name,
+      format: 'cjs',
+      sourcemap: true
+    }
   ],
   plugins: [
     del({ targets: 'dist/*', runOnce: true }),
@@ -35,7 +37,15 @@ export default {
     nodePolyfills(),
     typescript({ abortOnError: true, sourcemap: true, useTsconfigDeclarationDir: true }),
     commonjs(),
-    json()
+    json(),
+    copy({
+      targets: [
+        {
+          src: './package.json',
+          dest: './dist'
+        }
+      ]
+    })
   ],
   external: [...Object.keys(packageJson.dependencies || {}), ...Object.keys(packageJson.peerDependencies || {})]
 };
